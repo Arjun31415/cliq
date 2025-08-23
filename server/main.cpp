0f8e9192def93371a7bfdbef9a049b9bafec9b6f
Arjun31415 <arjunp0710@gmail.com>
2025-08-23 19:37:43 +0530

    fixed bug

--- server/main.cpp
+++ server/main.cpp
@@ -155,85 +155,86 @@ int main(int argc, char **argv) {
   // 3. EVENT LOOP:
   while (!g_terminate.load()) {
     int n = epoll_wait(epfd, events, max_events, -1);
+    LOG_DEBUG(g_logger, "num events: {}", n);
     if (n == -1) {
       if (errno == EINTR)
         continue; // interrupted by signal, retry
       LOG_ERROR(g_logger, "Error during epoll wait: {} (errno={})",
                 strerror(errno), errno);
       break; // need to cleanup
+    }
 
-      for (int i = 0; i < n; i++) {
-        int fd = events[i].data.fd;
-        if (fd == server_fd) {
-          // new connection
-          // Accept all incoming connections (ET means must loop)
-          while (true) {
-            struct sockaddr_in client_addr{};
-            socklen_t client_len = sizeof(client_addr);
-            int client_fd =
-                accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-            if (client_fd == -1) {
-              if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                // No more clients to accept
-                break;
-              } else {
-                LOG_ERROR(g_logger,
-                          "Error during accept connection: {} (errno={})",
-                          strerror(errno), errno);
-                break;
-              }
-            }
-
-            make_socket_nonblocking(client_fd);
-
-            struct epoll_event client_ev{};
-            client_ev.events = EPOLLIN | EPOLLET; // edge-triggered read
-            client_ev.data.fd = client_fd;
-            if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &client_ev) == -1) {
-              LOG_ERROR(g_logger,
-                        "Error during epoll_ctl: client_fd : {} (errno={})",
-                        strerror(errno), errno);
-              close(client_fd);
-              continue;
-            }
-
-            char client_ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &client_addr.sin_addr, client_ip,
-                      sizeof(client_ip));
-            LOG_INFO(g_logger, "Accepted connection from {}:{} (fd={})",
-                     client_ip, ntohs(client_addr.sin_port), fd);
-          }
-        } else {
-          // Handle client data (ET → must read until EAGAIN)
-          while (true) {
-            char buf[512];
-            ssize_t count = recv(fd, buf, sizeof(buf), 0);
-            if (count > 0) {
-              // Echo back
-              send(fd, buf, count, 0);
-            } else if (count == 0) {
-              // Client disconnected
-              LOG_INFO(g_logger, "Client (fd = {}) disconnected\n", fd);
-              close(fd);
+    for (int i = 0; i < n; i++) {
+      int fd = events[i].data.fd;
+      LOG_DEBUG(g_logger, "event fd:{} server fd:{}", fd, server_fd);
+      if (fd == server_fd) {
+        // new connection
+        // Accept all incoming connections (ET means must loop)
+        while (true) {
+          struct sockaddr_in client_addr{};
+          socklen_t client_len = sizeof(client_addr);
+          int client_fd =
+              accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+          if (client_fd == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+              // No more clients to accept
               break;
             } else {
-              if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                // No more data now
-                break;
-              } else {
-                perror("recv");
-                LOG_ERROR(g_logger, "Failed during data recieve: {} (errno={})",
-                          strerror(errno), errno);
-                close(fd);
-                break;
-              }
+              LOG_ERROR(g_logger,
+                        "Error during accept connection: {} (errno={})",
+                        strerror(errno), errno);
+              break;
+            }
+          }
+
+          make_socket_nonblocking(client_fd);
+
+          struct epoll_event client_ev{};
+          client_ev.events = EPOLLIN | EPOLLET; // edge-triggered read
+          client_ev.data.fd = client_fd;
+          if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &client_ev) == -1) {
+            LOG_ERROR(g_logger,
+                      "Error during epoll_ctl: client_fd : {} (errno={})",
+                      strerror(errno), errno);
+            close(client_fd);
+            continue;
+          }
+
+          char client_ip[INET_ADDRSTRLEN];
+          inet_ntop(AF_INET, &client_addr.sin_addr, client_ip,
+                    sizeof(client_ip));
+          LOG_INFO(g_logger, "Accepted connection from {}:{} (fd={})",
+                   client_ip, ntohs(client_addr.sin_port), fd);
+        }
+      } else {
+        // Handle client data (ET → must read until EAGAIN)
+        while (true) {
+          char buf[512];
+          ssize_t count = recv(fd, buf, sizeof(buf), 0);
+          if (count > 0) {
+            // Echo back
+            send(fd, buf, count, 0);
+          } else if (count == 0) {
+            // Client disconnected
+            LOG_INFO(g_logger, "Client (fd = {}) disconnected\n", fd);
+            close(fd);
+            break;
+          } else {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+              // No more data now
+              break;
+            } else {
+              perror("recv");
+              LOG_ERROR(g_logger, "Failed during data recieve: {} (errno={})",
+                        strerror(errno), errno);
+              close(fd);
+              break;
             }
           }
         }
       }
     }
   }
-
 
   LOG_DEBUG(g_logger, "Shutting down server gracefully");
   close(server_fd);
